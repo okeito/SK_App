97baef6af23450f001f7272e29a35150b6715dbd
okeito <okeito.s@gmail.com>
2014-03-09 18:10:28 +1100

    Refactoring!
    Added an Notification listener to that updates the view to display the
    weather condidtion or an error message. Swtiched the 'switch statement
    out for a Nsdictionary approach. Removel the modal aspect of the code
    into seperte files. in the process lost all animations, which will need
    to be added back in a new fashion.

---
+++ St Kilda App/St Kilda App/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  St Kilda App
+//
+//  Created by Okeito on 11/9/13.
+//  Copyright (c) 2013 Okeito. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ St Kilda App/St Kilda AppTests/St_Kilda_AppTests.m
@@ -0,0 +1,34 @@
+//
+//  St_Kilda_AppTests.m
+//  St Kilda AppTests
+//
+//  Created by Okeito on 11/9/13.
+//  Copyright (c) 2013 Okeito. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface St_Kilda_AppTests : XCTestCase
+
+@end
+
+@implementation St_Kilda_AppTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
